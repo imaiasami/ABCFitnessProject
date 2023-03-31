8b22a22465208155d3d3a64cb1a2338319a63db5
minji <alswl6915@gmail.com>
2023-03-31 13:53:27 +0900

    Memer 생성 /회원가입 / 로그인 html

---
+++ Fitness/src/main/java/com/project/file/member/Member.java
@@ -0,0 +1,40 @@
+package com.project.file.member;
+
+import java.util.Date;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+public class Member {
+	private long mem_no;
+	private  String mail;
+	private String password;
+	private String mem_id;
+	private String language;
+	private  int status;
+	private String name;
+	private Date birth;
+	private String gender;
+	private String phone;
+	private String image;
+	private String intro;
+	private double height;
+	private int skill;
+	private int days;
+	private int cardio;
+
+
+
+public Member(long mem_no, String mail, String mem_id, String password, String name, Date birth, String gender) {
+		this.mem_no = mem_no;
+		this.mail = mail;
+		this.mem_id = mem_id;
+		this.password = password;
+		this.name = name;
+		this.birth = birth;
+		this.gender = gender;
+	}
+
+}
