667ae2fc17ac060b949f34333cf57f7ab72be2a7
allofuni <kd915@yonsei.ac.kr>
2023-04-10 09:11:16 +0900

    로그인 여부에 따른 메인페이지 변경, 로그아웃 구현, 프로필 링크 연결, theme.css 분리

--- Fitness/src/main/java/com/project/file/controller/MemberController.java
+++ Fitness/src/main/java/com/project/file/controller/MemberController.java
@@ -23,64 +23,69 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 public class MemberController {
 	private final MemberRepository memberMapper;
-	
+
 	// 회원가입 폼 이동
-		@GetMapping("join")
-		public String joinMemberForm(Model model) {
-			model.addAttribute("memberJoin", new Member());
-			return "login/join";
-		}
+	@GetMapping("join")
+	public String joinMemberForm(Model model) {
+		model.addAttribute("memberJoin", new Member());
+		return "login/join";
+	}
 
-		// 회원가입
-		@PostMapping("join")
-		public String joinMember(@ModelAttribute("memberJoin") Member member) {
-			memberMapper.joinMember(member);
-			return "redirect:/";
-		}
+	// 회원가입
+	@PostMapping("join")
+	public String joinMember(@ModelAttribute("memberJoin") Member member) {
+		memberMapper.joinMember(member);
+		return "redirect:/";
+	}
 
-		// 로그인 폼으로 이동
-		@GetMapping("login")
-		public String loginMemberForm(Model model) {
-			model.addAttribute("memberLogin", new Member());
+	// 로그인 폼으로 이동
+	@GetMapping("login")
+	public String loginMemberForm(Model model) {
+		model.addAttribute("memberLogin", new Member());
+		return "login/login";
+	}
+
+	// 로그인하기
+	@PostMapping("login")
+	public String loginMember(@Validated @ModelAttribute("memberLogin") Member memberLogin, BindingResult result, HttpServletRequest request, @RequestParam String redirectURL) {
+		// 로그인 폼 유효성 검사
+		if (result.hasErrors()) {
 			return "login/login";
 		}
 
-		// 로그인하기
-		@PostMapping("login")
-		public String loginMember(@Validated @ModelAttribute("memberLogin") Member memberLogin, 
-				BindingResult result, HttpServletRequest request, @RequestParam String redirectURL) {
-		// 로그인 폼 유효성 검사
-		if (result.hasErrors()) {
-		return "login/login";
-		}
-
 		// 로그인 처리 로직
-		Member findMember = memberMapper.findMemberById(memberLogin.getMail());
+		Member findMember = memberMapper.findMemberByMail(memberLogin.getMail());
 		if (findMember == null || !findMember.getPassword().equals(memberLogin.getPassword())) {
-		result.reject("loginError", "아이디 또는 패스워드가 다릅니다.");
-		return "login/login";
-		}else {
-		// 로그인 성공 처리
-		HttpSession session = request.getSession();
-		session.setAttribute("memberLogin", findMember);
+			result.reject("loginError", "아이디 또는 패스워드가 다릅니다.");
+			return "login/login";
+		} else {
+			// 로그인 성공 처리
+			HttpSession session = request.getSession();
+			session.setAttribute("memberLogin", findMember);
 		}
 		return "redirect:/" + redirectURL;
-			
-		}
-		
-		
-		//비밀번호 찾기폼으로 이동
-		@GetMapping("password")
-		public String searchPasswordForm(Model model) {
-			model.addAttribute("memberPassword", new Member());
-			return "login/findpassword";
-		}
-		
-		//비밀번호 찾기
-		@PostMapping("password")
-		public String searchPassword(@ModelAttribute("memberPassword")Member memberPassword) {
-			
-			return  "login/login";
-		}
-		
+	}
+	
+	// 로그아웃
+	@GetMapping("logout")
+	public String logoutMember(HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		session.setAttribute("memberLogin", null);
+		return "redirect:/";
+	}
+
+	// 비밀번호 찾기폼으로 이동
+	@GetMapping("password")
+	public String searchPasswordForm(Model model) {
+		model.addAttribute("memberPassword", new Member());
+		return "login/findpassword";
+	}
+
+	// 비밀번호 찾기
+	@PostMapping("password")
+	public String searchPassword(@ModelAttribute("memberPassword") Member memberPassword) {
+
+		return "login/login";
+	}
+
 }
--- Fitness/src/main/resources/static/js/default.js
+++ Fitness/src/main/resources/static/js/default.js
@@ -12,4 +12,14 @@ $(() => {
     $('#etc-btn').click(() => {
         $('#etc-popup').toggleClass('open');
     });
+
+    $('#sidebar').hover(() => {
+        $('#sidebar').addClass('hosc')
+    }, () => {
+        $('#sidebar').removeClass('hosc')
+    });
+
+    if ($('#login-btn')) {
+        $('#login-btn > a').attr('href', '/login?redirectURL=' + location.pathname.slice(1));
+    }
 });
