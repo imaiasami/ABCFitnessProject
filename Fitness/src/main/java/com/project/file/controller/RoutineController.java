6269afb3e965c69c71a7f10f57088b06d02b098c
ImaiAsami <ydonghwan299@gmail.com>
2023-04-12 11:22:45 +0900

    기본 루틴 페이지 페이징 처리

--- Fitness/src/main/java/com/project/file/controller/RoutineController.java
+++ Fitness/src/main/java/com/project/file/controller/RoutineController.java
@@ -4,16 +4,19 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.ibatis.session.RowBounds;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.project.file.model.entity.exercise.Exercise;
 import com.project.file.model.entity.routine.RoutineDefault;
 import com.project.file.repository.ExerciseRepository;
 import com.project.file.repository.RoutineRepository;
+import com.project.file.util.PageNavigator;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,13 +26,25 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("routine")
 @Controller
 public class RoutineController {
-	
+
 	private final RoutineRepository routineMapper;
 	private final ExerciseRepository exerciseMapper;
-	
+
+	// 페이징 처리 상수값
+	final int countPerPage = 9; // 페이지 당 글의 수
+	final int pagePerGroup = 5; // 페이지 그룹 당 표시할 페이지 수
+
 	@GetMapping("default")
-	public String defaultRoutine(Model model) {
-		List<RoutineDefault> routines = routineMapper.getRoutineDefaultsKo();
+	public String defaultRoutine(Model model, @RequestParam(defaultValue = "1") int page) {
+
+		int total = routineMapper.getTotal();
+
+		// 페이징 처리를 위한 네비게이터 객체 생성
+		PageNavigator navigator = new PageNavigator(countPerPage, pagePerGroup, page, total);
+		model.addAttribute("navigator", navigator);
+		RowBounds rowBounds = new RowBounds(navigator.getStartRecord(), navigator.getCountPerPage());
+		
+		List<RoutineDefault> routines = routineMapper.getRoutineDefaultsKo(rowBounds);
 		model.addAttribute("routines", routines);
 		return "routine/defaultRoutine";
 	}
@@ -42,7 +57,7 @@ public class RoutineController {
 		model.addAttribute("exMap", exMap);
 		return "routine/defaultRoutineDetail";
 	}
-	
+
 	@GetMapping("themeRoutine")
 	public String themeRoutine() {
 		return "routine/themeRoutine";
@@ -50,10 +65,10 @@ public class RoutineController {
 
 	@GetMapping("themeRoutine/{rout_no}")
 	public String themeRoutineDetail(@PathVariable long rout_no) {
-		
+
 		return "routine/themeRoutineDetail";
 	}
-	
+
 	public Map<Long, Exercise> getExMap(List<Map<Long, Integer>> step) {
 		Map<Long, Exercise> result = new HashMap<>();
 		for (Map<Long, Integer> map : step) {
--- Fitness/src/main/java/com/project/file/repository/RoutineRepository.java
+++ Fitness/src/main/java/com/project/file/repository/RoutineRepository.java
@@ -3,13 +3,14 @@ package com.project.file.repository;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.session.RowBounds;
 
 import com.project.file.model.entity.routine.RoutineDefault;
 
 @Mapper
 public interface RoutineRepository {
 	// 전체 기본 루틴 가져오기
-	List<RoutineDefault> getRoutineDefaultsKo();
+	List<RoutineDefault> getRoutineDefaultsKo(RowBounds rowBounds);
 	List<RoutineDefault> getRoutineDefaultsJp();
 	List<RoutineDefault> getRoutineDefaultsEn();
 	
@@ -17,4 +18,7 @@ public interface RoutineRepository {
 	RoutineDefault getRoutineDefaultKo(long rout_no);
 	RoutineDefault getRoutineDefaultJp(long rout_no);
 	RoutineDefault getRoutineDefaultEn(long rout_no);
+	
+	// 총 루틴 개수
+	int getTotal();
 }
